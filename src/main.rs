9050bbec46f5bd8645c7c3681cbd2b425ae4c459
jackdfranklin <jackfranklin314@gmail.com>
2025-12-16 22:59:52 +0000

    Add the first plankton!

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
-        commands.spawn(Camera2d); 
+#[derive(Component)]
+#[require(Transform)]
+struct Plankton;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(15.0))),
+        MeshMaterial2d(materials.add(Color::srgb(1.0, 0.5, 0.5))),
+        Plankton,
+    ));
 }
