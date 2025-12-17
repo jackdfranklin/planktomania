a7c7aaff68c351d8a196d9aeae6c09fc58325886
jackdfranklin <jackfranklin314@gmail.com>
2025-12-17 09:08:36 +0000

    Add player component, spawn other plankton randomly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::{Rng};
 
 fn main() {
     App::new()
@@ -11,15 +12,37 @@ fn main() {
 #[require(Transform)]
 struct Plankton;
 
+#[derive(Component)]
+struct Player;
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let mut rng = rand::rng();
     commands.spawn(Camera2d);
+    for _ in 0..100 {
+        commands.spawn((
+            Mesh2d(meshes.add(Circle::new(15.0))),
+            MeshMaterial2d(materials.add(Color::srgb(1.0, 0.5, 0.5))),
+            Transform::from_xyz(
+                (rng.random::<f32>() - 0.5) * 1200.0,
+                (rng.random::<f32>() - 0.5) * 600.0,
+                0.0
+            ),
+            Plankton,
+        ));
+    }
     commands.spawn((
         Mesh2d(meshes.add(Circle::new(15.0))),
-        MeshMaterial2d(materials.add(Color::srgb(1.0, 0.5, 0.5))),
+        MeshMaterial2d(materials.add(Color::srgb(0.5, 0.5, 1.0))),
+        Transform::from_xyz(
+            (rng.random::<f32>() - 0.5) * 1200.0,
+            (rng.random::<f32>() - 0.5) * 600.0,
+            0.0
+        ),
         Plankton,
+        Player,
     ));
 }
