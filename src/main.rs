b141b83c9025eeef5e4a502618238e99241d43e8
jackdfranklin <jackfranklin314@gmail.com>
2025-12-18 14:34:33 +0000

    Add keyboard input for player movement

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,25 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(PreUpdate, accumulate_input)
+        .add_systems(FixedUpdate, update_positions)
         .run();
 }
 
 #[derive(Component)]
-#[require(Transform)]
 struct Plankton;
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+struct AccumulatedInput {
+    movement: Vec2,
+}
+
 
 fn setup(
     mut commands: Commands,
@@ -34,6 +44,7 @@ fn setup(
             Plankton,
         ));
     }
+    // Spawn the player
     commands.spawn((
         Mesh2d(meshes.add(Circle::new(15.0))),
         MeshMaterial2d(materials.add(Color::srgb(0.5, 0.5, 1.0))),
@@ -44,5 +55,44 @@ fn setup(
         ),
         Plankton,
         Player,
+        Velocity::default(),
+        AccumulatedInput::default(),
     ));
+}
+
+fn accumulate_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    player: Single<(&mut AccumulatedInput, &mut Velocity)>,
+) {
+    let (mut input, mut velocity) = player.into_inner();
+    
+    input.movement = Vec2::ZERO;
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        input.movement.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        input.movement.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        input.movement.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        input.movement.x += 1.0;
+    }
+
+    velocity.0 = 100.0 * input.movement.normalize_or_zero();
+}
+
+fn update_positions(
+    fixed_time: Res<Time<Fixed>>,
+    mut query: Query<(
+        &mut Transform,
+        &Velocity,
+    )>,
+) {
+
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.0.x * fixed_time.delta_secs();
+        transform.translation.y += velocity.0.y * fixed_time.delta_secs();
+    }
 }
